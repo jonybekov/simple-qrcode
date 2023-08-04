de17212e7a13eefe963c67c8ed016f4e66dfe2e7
R4Y <39439363+jonybekov@users.noreply.github.com>
2023-08-04 18:08:46 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,11 @@ import "./styles.css";
 import { QRCodeGenerator } from "./QRCodeGenerator";
 
 export default function App() {
-  const params = new URLSearchParams();
+  const params = new URLSearchParams(window.location.search);
   const trackingId = params.get("tracking_id");
   const userId = params.get("user_id");
   const width = params.get("width") || 500;
-
-  console.log(trackingId, width);
+  const isNumber = params.get("is_number") || 0;
 
   return (
     <div className="App">
@@ -18,7 +17,9 @@ export default function App() {
         data={
           trackingId
             ? {
-                tracking_id: trackingId
+                tracking_id: !!Number(isNumber)
+                  ? Number(trackingId)
+                  : trackingId
               }
             : userId
             ? {
